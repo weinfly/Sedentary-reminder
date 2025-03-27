86a33e60e4d5efc2dd278cef58a6feda04ab98e7
justfly <50286540+weinfly@users.noreply.github.com>
2025-03-27 14:23:15 +0800

    代码无更新，只是更新了显示的版本号

--- Reminder/AboutBox.Designer.cs
+++ Reminder/AboutBox.Designer.cs
@@ -55,7 +55,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 18);
             this.label2.TabIndex = 1;
-            this.label2.Text = "v1.4.0";
+            this.label2.Text = "v1.4.1";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
